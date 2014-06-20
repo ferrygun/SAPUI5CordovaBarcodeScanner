56722d0ea974186abcd66a3c906af21dd4ce4f75
ferrygun <ferrygun@yahoo.com>
2014-06-20 21:50:41 +0800

    Create Component.js

---
+++ app/Component.js
@@ -0,0 +1,13 @@
+jQuery.sap.declare("app.Component");
+
+jQuery.sap.require("sap.ui.model.resource.ResourceModel");
+
+sap.ui.core.UIComponent.extend("app.Component", {
+
+    createContent: function() {
+
+        // since v0.7.0 the Component is not needed anymore
+        // we keep it for your project specific usage
+
+    }
+});
